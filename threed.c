598aae6b8e41d65ef28a700c7cccbe7e4b66d95d
Mani kiran <m.manikiran@yahoo.com>
2018-03-15 21:10:48 +0000

    cube

--- threed.c
+++ threed.c
@@ -1,6 +1,26 @@
 #include <GL/glut.h>
 #include <stdio.h>
 
+void display(){
+	
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-3.2, 3.4, -2.4, 2.4, 1, 50);
+
+	glMatrixMode(GL_MODELVIEW);
+	gluLookAt(4, 0, 0, 0, 1, 0, 0, 1, 0);
+	
+	glColor3f(1.0, 1.0, 0.0);
+	
+	glPushMatrix();
+	//glTranslated(0.5, 0.5, 0.5);
+	
+	glutSolidCube(1.0);
+	glPopMatrix();
+	glFlush();
+}
+
 int main(int argc, char** argv){
 
 	glutInit(&argc, argv);
@@ -9,12 +29,11 @@ int main(int argc, char** argv){
 	glutInitWindowPosition(100, 150);
 	glutCreateWindow("Sample 3D image");
 	
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho(-3.2, 3.4, -2.4, 2.4, 1, 50);
-
-	glMatrixMode(GL_MODELVIEW);
-	glLookAt(4, 4, 4, 0, 1, 0, 0, 1, 0);
-
+	glutDisplayFunc(display);
 	
-}
+	
+//	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glViewport(0, 0, 640, 480);
+	glutMainLoop();
+	
+}	
